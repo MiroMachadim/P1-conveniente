75731c8ea3fa9d8644389f9f1e9e01a2c5a59892
MiroMachadim <miromachadodnv@gmail.com>
2025-03-20 18:15:04 -0300

    tradutor binario.py

--- tradutor binario.py
+++ tradutor binario.py
@@ -1,11 +1,10 @@
-
 soma = 0
 x = input('seu número binário')
-for k in range(0,len(x)):
-    if x[k] == '1':
+for k in range(1,len(x)+1):
+    if x[-k] == '1':
         convertion = 1
     else:
         convertion = 0
-    decimal = (2**k) * convertion
+    decimal = (2**(k-1)) * convertion
     soma += decimal
 print(soma)
