d1b03ee5a06afae6caac62fadce98855f5f0f010
MiroMachadim <miromachadodnv@gmail.com>
2025-03-28 10:03:20 -0300

    reescrevendoEquação.py

--- reescrevendoEquação.py
+++ reescrevendoEquação.py
@@ -4,16 +4,27 @@ def dANDr(x):
     numberscount = []
     misc = []
     signal = []
+    numberscountcounter = -1
     #pegando as informações
     for Ascii in x:
         if Ascii == '²' or Ascii == f'(' or Ascii == f')':
             misc.append(Ascii)
+            switch = 0
         elif Ascii == '+' or Ascii == '-':
             signal.append(Ascii)
-        elif Ascii.isdigit():
+            switch = 0
+        elif Ascii.isdigit() and switch == 0:
             numberscount.append(Ascii)
+            numberscountcounter+=1
+            switch = 1
+        elif Ascii.isdigit() and switch == 1:
+            new = numberscount[numberscountcounter] + Ascii
+            numberscount.pop()
+            numberscount.append(new)
+            switch = 0
         elif Ascii.isalpha():
             letters.append(Ascii)
+            switch = 0
     #a,b,c
     a = int(signal[0]+numberscount[0])
     if len(numberscount) >= 2:
@@ -23,14 +34,16 @@ def dANDr(x):
     #detectando que é segundo grau,ax²+bx+c
     if len(letters) == 2 and len(numberscount) >= 3 and len(misc) == 1:
         delta = b**2 - 4 * a * c
-        if delta <= 0:
+        if delta < 0:
             return x
         x1 = (-b+(delta**(1/2)))/(2*a)
         x2 = (-b-(delta**(1/2)))/(2*a)
-        return f'{a}({letters[0]}-{round(x1,2)})*({letters[0]}-{round(x2,2)})'
+        return f'{a}({letters[0]}-({round(x1,2)}))*({letters[0]}-({round(x2,2)}))'
     #detectando que é quadrado da diferença,x²-a
     if len(letters) == 1 and len(misc) == 1 and len(numberscount) == 1 and (int(numberscount[0]) ** (1/2)) % 1 == 0:
-        return f'({letters[0]}+{a**(1/2)}) * ({letters[0]}-{a**(1/2)})'
+        if a > 0:
+            return x
+        return f'({letters[0]}+{(-a)**(1/2)}) * ({letters[0]}-{(-a)**(1/2)})'
     #detectando que é produto notável,(x+a)²
     if len(letters) == 1 and len(misc) == 3 and len(numberscount) == 1:
         return f'{letters[0]}² + 2*{letters[0]}*{a} +{a}²'
